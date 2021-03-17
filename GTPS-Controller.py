1c586f0c5502c43707d5d64a131671149b3fca0d
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-03-17 17:30:08 +0700

    Update GTPS-Controller.py

--- GTPS-Controller.py
+++ GTPS-Controller.py
@@ -50,6 +50,30 @@ async def world(ctx):
     listworld = len(os.listdir('worlds'))
     await ctx.send(f"{listworld} Created For Now")
 
+@bot.command()
+async def givegem(ctx, args1, args2):
+    task1 = open(f'C:\\yourfoldergems\\{args1}', 'r').read();
+    oldgems = re.findall("irfan: (.+?)",str(task1))[0]
+
+    givegems = task1.replace(oldgems, args2);
+
+    task2 = open(f'C:\\yourfoldergems\\{args1}', 'w')
+    task2.write(givegems)
+    task2.close()
+    await ctx.send(f"Gems already added to\nPlayer: {args1}\nAmount: {args2}")
+
+@bot.command()
+async def givelevel(ctx, args1, args2):
+    task1 = open(f'C:\\yourfolderlevel\\{args1}', 'r').read();
+    oldlevel = re.findall("levelplayer: (.+?)",str(task1))[0]
+
+    newlevel = task1.replace(oldlevel, args2);
+
+    task2 = open(f'C:\\yourfolderlevel\\{args1}', 'w')
+    task2.write(newlevel)
+    task2.close()
+    await ctx.send(f"New Level already added to\nPlayer: {args1}\nAmount: {args2}")
+
 # @bot.command()
 # async def help(ctx):
 #     time = datetime.now()
